ecdd9d4207076a528c1943c1c399d20bb1dfd843
erudo <hashierudo@gmail.com>
2020-11-27 22:11:03 +0900

    [add] add SummonTask
    Bossを召喚するためのTaskを追加

--- src/main/java/net/erudosaba/mc/emineboss/Main.java
+++ src/main/java/net/erudosaba/mc/emineboss/Main.java
@@ -1,10 +1,17 @@
 package net.erudosaba.mc.emineboss;
 
+import lombok.Getter;
+import net.erudosaba.mc.emineboss.util.config.Config;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     public static final String PLUGIN_NAME = "EMine-Boss";
+
+    private Main instance;
+
+    @Getter
+    private Config myConfig;
 
     @Override
     public void onDisable() {
@@ -13,7 +20,18 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        setInstance(this);
         getLogger().info("Enabled " + PLUGIN_NAME);
+
+        myConfig = new Config(getInstance());
+    }
+
+    private Main getInstance() {
+        return this.instance;
+    }
+
+    private void setInstance(Main instance) {
+        this.instance = instance;
     }
 
 
---
+++ src/main/java/net/erudosaba/mc/emineboss/task/SummonTask.java
@@ -0,0 +1,11 @@
+package net.erudosaba.mc.emineboss.task;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class SummonTask extends BukkitRunnable {
+
+    @Override
+    public void run() {
+
+    }
+}
